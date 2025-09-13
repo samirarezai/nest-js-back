668f2297fc6a72873197be50fc40b7053c2cac68
Samira Rezayi <43062463+samirarezai@users.noreply.github.com>
2025-09-13 20:25:24 +0330

    feat: add Users module and controller with basic user retrieval

---
+++ src/users/users.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+
+    @Get()
+    getAllUsers() {
+      return [
+        {
+            username: 'john_doe',
+            email: 'john@example.com'
+        }
+      ]
+    }
+}

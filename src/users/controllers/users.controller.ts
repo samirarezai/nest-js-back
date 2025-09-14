4964836f9dff0a91e103203e156f134ba3bcd2b8
Samira Rezayi <43062463+samirarezai@users.noreply.github.com>
2025-09-14 19:27:39 +0330

    style: format code in UsersController for improved readability

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -3,13 +3,13 @@ import { Controller, Get } from '@nestjs/common';
 @Controller('users')
 export class UsersController {
 
-    @Get()
-    getAllUsers() {
-      return [
-        {
-            username: 'john_doe',
-            email: 'john@example.com'
-        }
-      ]
-    }
+  @Get()
+  getAllUsers() {
+    return [
+      {
+        username: 'john_doe',
+        email: 'john@example.com'
+      }
+    ]
+  }
 }
